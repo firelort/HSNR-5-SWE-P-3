89bb37cf785da687effc93b0c5a0b4c2c539fcea
Alexander Niersmann <aniersmann@gmail.com>
2020-01-19 18:02:50 +0100

    switched to async sqlite

--- src/Database/Connecion.js
+++ src/Database/Connecion.js
@@ -1,15 +1,15 @@
-let sqlite3 = require('sqlite3').verbose(); // Variante mit Debugging-Informationen
+let Database = require('sqlite-async'); // Variante mit Debugging-Informationen
 
-let connectedDatabase = (pathToFile) => {
-    let db = new sqlite3.Database(pathToFile, (error) => {
-        if (error) {
-            console.error(error.message);
-            throw error;
-        } else {
-            console.log('Connected to the SQlite database.');
-            return db;
-        }
-    });
+let connectedDatabase = async (pathToFile) => {
+    try {
+        let db = Database.open(pathToFile);
+        console.log('Connected to the SQlite database.');
+        return db;
+    } catch (e) {
+        console.error(e.message);
+        throw e;
+    }
+
 };
 
 
